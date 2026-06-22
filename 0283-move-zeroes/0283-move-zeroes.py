37910e9969105cb149ee32b25c15cd659f13dead
Vedansh Dwivedi <vedanshdwivedi0@gmail.com>
2026-06-22 22:06:00 +0530

    Time: 4 ms (59.75%), Space: 20.4 MB (61.22%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,14 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nz_index = 0
-        cur_index = 0
-        while cur_index < len(nums):
+        nz_index = cur_index = 0
+        max_index = len(nums)
+        while cur_index < max_index:
             if nums[cur_index] != 0:
                 nums[nz_index] = nums[cur_index]
                 nz_index += 1
             cur_index += 1
-        for i in range(nz_index, len(nums)):
+        for i in range(nz_index, max_index):
             nums[i] = 0
 
         
