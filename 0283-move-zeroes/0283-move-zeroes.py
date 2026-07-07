228249cce78e1b189a2b505f89c2acc14c636bd5
Vedansh Dwivedi <vedanshdwivedi0@gmail.com>
2026-07-07 20:06:09 +0530

    Time: 5 ms (50.08%), Space: 20.4 MB (89.09%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,16 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        curEligibleIndex = 0
-        curIndex = 0
-        while curIndex < len(nums):
-            if nums[curIndex] != 0:
-                if curEligibleIndex != curIndex:
-                    nums[curEligibleIndex] = nums[curIndex]
-                curEligibleIndex += 1
-            curIndex += 1
-        for i in range(curEligibleIndex, len(nums)):
-            nums[i] = 0
+        slow = 0
+
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
 
 
         
