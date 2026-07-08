c5cb3e6031f1e78daea412d42eb2783a04a9f514
Vedansh Dwivedi <vedanshdwivedi0@gmail.com>
2026-07-09 01:56:06 +0530

    Time: 511 ms (85.35%), Space: 22.4 MB (18.45%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,9 +3,7 @@ class Solution:
         nums = sorted(nums)
         triplets = []
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            if nums[i] > 0:
+            if (i > 0 and nums[i] == nums[i - 1]) or nums[i] > 0:
                 continue
             left = i + 1
             right = len(nums) - 1
