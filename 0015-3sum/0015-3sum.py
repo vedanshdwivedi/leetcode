dad9b791222f250020c9f91c22921d43ae1cd699
Vedansh Dwivedi <vedanshdwivedi0@gmail.com>
2026-07-09 01:54:04 +0530

    Time: 647 ms (48.16%), Space: 22.2 MB (81.41%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,11 +2,9 @@ class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums = sorted(nums)
         triplets = []
-        seen = {}
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            seen[nums[i]] = True
             left = i + 1
             right = len(nums) - 1
             while left < right:
