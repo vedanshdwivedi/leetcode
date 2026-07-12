4436a9e41bf64218eba10fbf0ce9dcc6f16ea5fe
Vedansh Dwivedi <vedanshdwivedi0@gmail.com>
2026-07-13 02:37:22 +0530

    Time: 0 ms (100%), Space: 19.3 MB (60.75%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,4 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        eligibleIndex = 0
+        left = 0
+        for i in range(left, len(nums)):
+            if nums[i] == 0:
+                nums[eligibleIndex], nums[i] = 0, nums[eligibleIndex]
+                eligibleIndex += 1
+        left = eligibleIndex
+        # print(nums)
+        for i in range(left, len(nums)):
+            if nums[i] == 1:
+                nums[eligibleIndex], nums[i] = 1, nums[eligibleIndex]
+                eligibleIndex += 1
+        
+#         0 1 2 3 4 5
+# nums = [0,1]
+# eligibleIndex = 0
+# left = 1
+# i = 1 (1,2,3,4,5)
+
